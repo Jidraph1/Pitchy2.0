6933a2a3bd5fb8b4e90628ce3ecb1b6447b6b1a2
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-09 00:29:17 +0300

    flaskform classes

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,4 +4,22 @@ from wtforms.validators import InputRequired, Email
 from wtforms.widgets import TextArea
 
 class LoginForm(FlaskForm):
-    sd
+    username = StringField('Username',validators=[InputRequired()])
+    password = PasswordField('Password',validators=[InputRequired()])
+    remember = BooleanField('Remember')
+    
+class RegisterForm(FlaskForm):
+    
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    username = StringField('username',validators=[InputRequired()])   
+    password = PasswordField('Password',validators=[InputRequired()])
+    
+    
+class PitchForm(FlaskForm):
+    
+    title = StringField('Title of pitch',validators=[InputRequired()])
+    category = StringField('Category: Innovation, Business or Pickup Line',validators=[InputRequired()])
+    description = StringField('Pitch',validators=[InputRequired()], widget=TextArea())
+    
+class CommentForm(FlaskForm):
+    content = StringField('Comment')   
