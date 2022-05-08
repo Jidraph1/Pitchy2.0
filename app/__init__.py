b7dcc5382562eee5dfd1fb6c7da71d5a9bd296a8
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-09 01:01:11 +0300

    index main page

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app(config_name):
     app = Flask(__name__)
 
     # Setting up configuration
-    app.config.from_object(DevConfig)
+    app.config.from_object(config_options[config_name])
 
     #initialising flask extensions
     bootstrap.init_app(app)
