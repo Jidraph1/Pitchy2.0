e644668dd01ed14c8cb065deecd6973de4349ae3
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-09 00:39:13 +0300

    signup function

--- app/main/views.py
+++ app/main/views.py
@@ -1,24 +1,27 @@
 from flask import render_template,request,redirect,url_for
 from flask_wtf import FlaskForm
+from .forms import LoginForm, RegisterForm, PitchForm, CommentForm
 from ..models import Users, Pitch, Comment
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
-from .. import main
-from . import db
+from . import main
+from .. import db
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
     title = "Home page"
     return render_template('index.html', title = title)
 
-@app.route('/signup')
+@main.route('/signup', methods=['GET', 'POST'])
 def signup():
-    """
-    View sign up page
-    """
-    return render_template('signup.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+       
+        new_user = Users(username=form.username.data, email=form.email.data, password=hashed_password)
+    
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/success')
+    return render_template('signup.html', form=form)
