40ab142905a56a58dfd8ee38caa3df2557ef2d7b
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-08 21:04:33 +0300

    one to many relationship

--- app/models.py
+++ app/models.py
@@ -6,9 +6,16 @@ class Users(db.Model):
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String)
     password = db.Column(db.String) 
+    #Relationship
     pitches = db.relationship('Pitch', backref='owner')
     comments = db.relationship('Comment', backref='owner')
     def __repr__(self):
         return f"User('{self.username}')"
 
-        
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    description = db.Column(db.String(), index=True)
+
