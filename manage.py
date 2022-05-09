615a8a6b7f9c412c5110c7381a931dfc7533834f
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-09 15:32:26 +0300

    added Migration

--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
 from app import create_app,db
+from  flask_migrate import Migrate, MigrateCommand
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
