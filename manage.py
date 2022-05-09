743d3142b9e0da011e7c16851705a4e4ca61b83a
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-09 18:34:07 +0300

    Migrations directory

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager, Server
 from  flask_migrate import Migrate, MigrateCommand
+from flask_script import Shell
 
 #creating app instance
 app = create_app('production')
@@ -10,6 +11,10 @@ manager.add_command('server',Server)
 
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Pitch=Pitch,Comment=Comment )
 
 if __name__ == '__main__':
     manager.run()
