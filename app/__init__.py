77972492f684c5d4597ebba9261948ca3f3f901d
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-05-10 12:45:56 +0300

    fix database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
+
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+
+
 
 login_manager = LoginManager()
 login_manager.login_view = 'login'
@@ -11,20 +14,20 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
-
-    # Initializing application
+    
     app = Flask(__name__)
 
-    # Setting up configuration
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
-    #initialising flask extensions
+    # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-
-    # Registering the blueprint
+    
+ # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
 
     return app
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from app import create_app,db
 from flask_script import Manager, Server
 from  flask_migrate import Migrate, MigrateCommand
 from flask_script import Shell
+from app.models import Users, Pitch, Comment
 
 #creating app instance
 app = create_app('production')
@@ -12,9 +13,9 @@ manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
-# @manager.shell
-# def make_shell_context():
-#     return dict(app = app,db = db,Users = Users, Pitch=Pitch,Comment=Comment )
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,Users = Users, Pitch=Pitch,Comment=Comment )
 
 if __name__ == '__main__':
     manager.run()
